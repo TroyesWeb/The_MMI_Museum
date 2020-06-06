b8144c862ebadf028affd1501a170304f1a4ff23
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-06-06 17:19:47 +0200

    * Avancée sur la page d'accueil

--- application/views/Accueil_view.php
+++ application/views/Accueil_view.php
@@ -1,28 +1,52 @@
 <?php $this->load->view('Head_view'); ?>
+<!-- Full Page Intro -->
+<div class="view" style="background-image: url('https://remeng.rosselcdn.net/sites/default/files/dpistyles_v2/ena_16_9_extra_big/2019/01/31/node_36158/3977620/public/2019/01/31/B9718433140Z.1_20190131213803_000%2BGG0CSM6AT.1-0.jpg?itok=_eiKH9Iw1548967091'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
+	<!-- Mask & flexbox options-->
+	<div class="mask rgba-black-light align-items-center">
 
-<div class="container">
-	<!-- Logos -->
-	<div class="row pt-5 align-middle">
-		<div class="col-lg col-sm-2">
-			<img class="img-fluid" src="<?php echo base_url()?>assets/photos/logonoiretblanc.png" alt="">
-		</div>
-		<div class="col-lg col-sm-2">
-			<img class="img-fluid" src="<?php echo base_url()?>assets/photos/logo_mmi_troyes.png" alt="">
-		</div>
-	</div>
+		<div class="container">
 
-	<!-- Description -->
-	<div class="row align-middle">
-		<div class="col">
-			<p class="text-center">
-				MMI Museum, site internet référençant les productions des étudiants en MMI à l'IUT de Troyes.
-			</p>
-			<p class="text-center font-italic">
-				Celles-ci sont principalement des sites internet, des productions graphiques, du web design ainsi que des productions audiovisuelles.
-			</p>
+			<!-- Logos -->
+			<div class="row pt-5 pb-5 align-middle logo-wrapper">
+				<div class="col-lg col-sm-2 ">
+					<img class="img-fluid" style="width: 100px" src="<?php echo base_url()?>assets/photos/logonoiretblanc.png" alt="">
+				</div>
+				<div class="col-lg col-sm-2">
+					<img class="img-fluid" style="width: 100px" src="<?php echo base_url()?>assets/photos/logo_mmi_troyes.png" alt="">
+				</div>
+			</div>
+
+			<!-- Description -->
+			<div class="row align-middle">
+				<div class="col">
+					<p class="text-center text-white">
+						MMI Museum, site internet référençant les productions des étudiants en MMI à l'IUT de Troyes.
+					</p>
+					<p class="text-center text-white font-italic">
+						Celles-ci sont principalement des sites internet, des productions graphiques, du web design ainsi que des productions audiovisuelles.
+					</p>
+				</div>
+			</div>
+
+			<!-- Boutons -->
+			<div class="row align-middle mb-3">
+				<div class="col pt-5">
+					<p class="text-center text-white font-weight-bolder">
+						Votre profil:
+					</p>
+					<p class="text-center text-white">
+						<button type="button" class="btn btn-lg purple lighten-2 btn-rounded">Étudiant</button>
+						<button type="button" class="btn btn-lg purple lighten-2 btn-rounded">Administrateur</button>
+					</p>
+				</div>
+			</div>
+
 		</div>
 	</div>
 </div>
 
 <?php $this->load->view('Footer_view'); ?>
 </html>
+
+
+<button type="button" class="btn btn-secondary btn-rounded">Secondary</button>
