f76345674eb4fc5245092d1b9c4df7f4f74fd076
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-06-06 16:36:39 +0200

    * Changement du base_url pour localhost/themmimuseum.ml * Activation du helper 'url' * Création de Head_view.php * Création de Accueil_view.php et Accueil.php

---
+++ application/controllers/Accueil.php
@@ -0,0 +1,10 @@
+<?php
+
+class Accueil extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('Accueil_view');
+	}
+}
+?>
---
+++ application/views/Accueil_view.php
@@ -0,0 +1,11 @@
+<?php $this->load->view('Head_view'); ?>
+<div class="container">
+	<div class="row">
+		<div class="col">
+			<img src="<?php echo base_url()?>assets/photos/logonoiretblanc.png" alt="">
+		</div>
+		<div class="col">
+			One of three columns
+		</div>
+	</div>
+</div>
