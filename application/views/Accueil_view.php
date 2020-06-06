be23e186316dae3ec23fcb4c6d54e06f7cb799e3
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-06-06 16:53:20 +0200

    * Avancée sur la page d'accueil * Footer_view.php créée

--- application/views/Accueil_view.php
+++ application/views/Accueil_view.php
@@ -1,11 +1,28 @@
 <?php $this->load->view('Head_view'); ?>
+
 <div class="container">
-	<div class="row">
-		<div class="col">
-			<img src="<?php echo base_url()?>assets/photos/logonoiretblanc.png" alt="">
+	<!-- Logos -->
+	<div class="row pt-5 align-middle">
+		<div class="col-lg col-sm-2">
+			<img class="img-fluid" src="<?php echo base_url()?>assets/photos/logonoiretblanc.png" alt="">
 		</div>
+		<div class="col-lg col-sm-2">
+			<img class="img-fluid" src="<?php echo base_url()?>assets/photos/logo_mmi_troyes.png" alt="">
+		</div>
+	</div>
+
+	<!-- Description -->
+	<div class="row align-middle">
 		<div class="col">
-			One of three columns
+			<p class="text-center">
+				MMI Museum, site internet référençant les productions des étudiants en MMI à l'IUT de Troyes.
+			</p>
+			<p class="text-center font-italic">
+				Celles-ci sont principalement des sites internet, des productions graphiques, du web design ainsi que des productions audiovisuelles.
+			</p>
 		</div>
 	</div>
 </div>
+
+<?php $this->load->view('Footer_view'); ?>
+</html>
