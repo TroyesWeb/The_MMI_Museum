d632d4af058abca78d72b5529b2d15ccd887cde3
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-06-06 17:36:46 +0200

    * Avancée sur la page d'accueil
    TODO: Aligner les logos

--- application/views/Accueil_view.php
+++ application/views/Accueil_view.php
@@ -2,12 +2,12 @@
 <!-- Full Page Intro -->
 <div class="view" style="background-image: url('https://remeng.rosselcdn.net/sites/default/files/dpistyles_v2/ena_16_9_extra_big/2019/01/31/node_36158/3977620/public/2019/01/31/B9718433140Z.1_20190131213803_000%2BGG0CSM6AT.1-0.jpg?itok=_eiKH9Iw1548967091'); background-repeat: no-repeat; background-size: cover; background-position: center center;">
 	<!-- Mask & flexbox options-->
-	<div class="mask rgba-black-light align-items-center">
+	<div class="mask rgba-black-light align-content-center align-items-center">
 
-		<div class="container">
+		<div class="container align-content-center align-items-center">
 
 			<!-- Logos -->
-			<div class="row pt-5 pb-5 d-flex justify-content-center text-center">
+			<div class="row py-5 d-flex justify-content-center text-center">
 				<div class="col col-md-2">
 					<img class="img-fluid" style="width: 13rem" src="<?php echo base_url()?>assets/photos/logonoiretblanc.png" alt="">
 					<img class="img-fluid" style="width: 13rem" src="<?php echo base_url()?>assets/photos/logo_mmi_troyes.png" alt="">
@@ -16,7 +16,7 @@
 
 			<!-- Description -->
 			<div class="row align-middle">
-				<div class="col">
+				<div class="col" id="description">
 					<p class="text-center text-white">
 						MMI Museum, site internet référençant les productions des étudiants en MMI à l'IUT de Troyes.
 					</p>
