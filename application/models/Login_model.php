a514c2960de1136260b5a58d2cbbf88b77e9424a
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-06-07 09:51:37 +0200

    * Indent Login_model.php

--- application/models/Login_model.php
+++ application/models/Login_model.php
@@ -9,7 +9,7 @@ class Login_model extends CI_Model{
 	public function verifLog($i,$p)
 	{
 		$query = $this->db->query("SELECT * FROM users WHERE identifiant='$i' AND password= sha2('$p',256)");
-return $query->num_rows();
-}
+		return $query->num_rows();
+	}
 
 }
