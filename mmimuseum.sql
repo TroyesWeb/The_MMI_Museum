1b22551f212049330e7bd8f0f2c0d2ab2d6f4c2a
Aloïs GAUCHER <30592770+couquino@users.noreply.github.com>
2020-06-08 21:05:18 +0200

    * Mise à jour de la base de données
    TODO: Ajouter un premier travail

--- mmimuseum.sql
+++ mmimuseum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  lun. 08 juin 2020 à 13:05
+-- Généré le :  lun. 08 juin 2020 à 15:49
 -- Version du serveur :  10.4.10-MariaDB
 -- Version de PHP :  7.3.12
 
@@ -30,12 +30,20 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
-  `admin_id` int(4) NOT NULL,
-  `admin_identifiant` int(40) NOT NULL,
-  `admin_password` int(100) NOT NULL,
-  `admin_nom` int(30) NOT NULL,
-  `admin_prenom` int(30) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  `admin_id` int(5) NOT NULL AUTO_INCREMENT,
+  `admin_identifiant` varchar(30) NOT NULL,
+  `admin_password` varchar(100) NOT NULL,
+  `admin_prenom` varchar(30) NOT NULL,
+  `admin_nom` varchar(30) NOT NULL,
+  PRIMARY KEY (`admin_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `admin`
+--
+
+INSERT INTO `admin` (`admin_id`, `admin_identifiant`, `admin_password`, `admin_prenom`, `admin_nom`) VALUES
+(1, 'admin', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', '', '');
 
 -- --------------------------------------------------------
 
@@ -68,7 +76,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `users_prenom` varchar(30) NOT NULL,
   `users_nom` varchar(30) NOT NULL,
   PRIMARY KEY (`users_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`users_id`, `identifiant`, `password`, `users_prenom`, `users_nom`) VALUES
+(1, 'test', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08', 'test', 'test');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
