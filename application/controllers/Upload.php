705387854a98f203677d996fc9ff3e52cf99f33c
jfryl <joffrey.lefebvre@etudiant.univ-reims.fr>
2020-06-09 07:24:24 +0200

    Ajout des fonctions pour envoyer à la base de données

--- application/controllers/Upload.php
+++ application/controllers/Upload.php
@@ -15,5 +15,36 @@ class Upload extends CI_Controller {
 				$this->load->view('Upload_view');
 			}
 		}
+
+	public function Envoi()
+	{
+
+		$intitule = $this->input->post('intitule');
+		$description = $this->input->post('description');
+		$photo = $this->input->post('photo');
+		$classe = $this->input->post('classe');
+		$date = $this->input->post('date');
+
+		$config['upload_path']          = base_url().'/assets/photos';
+		$config['allowed_types']        = 'gif|jpg|png';
+		$config['max_size']             = 100;
+		$config['max_width']            = 1024;
+		$config['max_height']           = 768;
+
+		$this->load->library('upload', $config);
+		$this->upload->initialize($config);
+
+
+		$this->load->model('Upload_model');
+
+		$upload = $this->Upload_model->Upload($intitule, $description, $photo, $classe, $date);
+		if ($upload=true){
+			redirect(base_url ().'Accueil');
+		}
+		else{
+			redirect (base_url ().'Upload');
+		}
+		// TODO: enregistrement de l'image dans /assets/photos
+	}
 }
 ?>
